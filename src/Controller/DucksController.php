5ad76d31e04db8c2a5b00642db4ef38961d8d4e5
ArnaudAZZOUZ <arnaud.azzouz-thuderoz@le-campus-numerique.fr>
2020-11-23 09:47:51 +0100

    edit user

---
+++ src/Controller/DucksController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Ducks;
+use App\Entity\Quack;
+use App\Form\DucksType;
+use App\Form\Quack1Type;
+use App\Repository\DucksRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+/**
+ * @Route("/ducks")
+ */
+class DucksController extends AbstractController
+{
+    /**
+     * @Route("/", name="ducks_index", methods={"GET"})
+     */
+    public function index(DucksRepository $ducksRepository): Response
+    {
+        return $this->render('ducks/index.html.twig', [
+            'ducks' => $ducksRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="duck_show", methods={"GET"})
+     */
+    public function show(Ducks $duck): Response
+    {
+        return $this->render('ducks/show.html.twig', [
+            'duck' => $duck,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="duck_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Ducks $duck): Response
+    {
+        $form = $this->createForm(DucksType::class, $duck);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('ducks_index');
+        }
+
+        return $this->render('ducks/edit.html.twig', [
+            'duck' => $duck,
+            'duck_form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="duck_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Ducks $duck): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$duck->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($duck);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('ducks_index');
+    }
+
+}
